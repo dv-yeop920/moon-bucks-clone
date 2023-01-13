9b11b9c3435a1af9c42ea9b084bed4c9eee7df4b
junyeoplee <jyeop920@gmail.com>
2023-01-14 03:10:23 +0900

    input add event
    강의에서 배우는 내용을 토대로
    내 방식으로 코드 수정
    인풋에 입력한 값을 submit 했을 때
    출력 값 확인 하는 과정

--- moonbucks-menu-main/src/js/index.js
+++ moonbucks-menu-main/src/js/index.js
@@ -1,6 +1,5 @@
 /* 요구 사항 
 step1 Todo 메뉴 추가
-[] 메뉴의 이름을 입력 받고 확인 버튼을 누르면 메뉴가 추가 된다. => input 에 입력 받고 submit 을 하면 게시판에 메뉴가 추가 된다
 [] 메뉴의 이름을 입력 받고 엔터 키를 치면 메뉴가 추가 된다
 [] 추가 되는 메뉴의 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"><ul/> 안에 삽입 되야 한다
 [] 총 메뉴 갯수를 count 하여 상단에 보여 준다 => count total
@@ -15,3 +14,28 @@ step3 Todo 메뉴 삭제
 [] 메뉴삭제 버튼 클릭 이벤트를 받고 , 메뉴 삭제 컨펌 모달창이 뜬다
 [] 확인 버튼을 클릭 하면 메뉴가 삭제 된다
 [] 총 메뉴 갯수를 count 하여 수정된 메뉴 갯수를 상태에 보여 준다. */
+
+const userInput = document.querySelector('#espresso-menu-name');
+const userSubmitButton = document.querySelector('#espresso-menu-submit-button');
+const menuForm = document.querySelector('#espresso-menu-form');
+
+const app = function() {
+    //form 태그가 자동으로 전송 되는 것을 막아 준다
+    menuForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+    })
+
+    userInput.addEventListener('keypress' , (event) => {
+        if(event.key === 'Enter'){
+            console.log(userInput.value);
+        }
+    });
+
+    userSubmitButton.addEventListener('click' , (event) => {
+        while(userInput){
+            console.log(userInput.value);
+            break;
+        }
+    });
+}
+app();
