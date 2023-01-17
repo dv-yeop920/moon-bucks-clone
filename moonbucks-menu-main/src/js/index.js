522745fdfdfb505eeb839f9a13e241fb90eb9cec
junyeoplee <jyeop920@gmail.com>
2023-01-17 19:08:07 +0900

    menu list 갯수에 따른 카운트
    메뉴 리스트(ul태그) 안에 등록 된 li 태그의 갯수를 파악하여 그 갯수만큼 카운트 되어 몇개의 메뉴가 있는지 출력

--- moonbucks-menu-main/src/js/index.js
+++ moonbucks-menu-main/src/js/index.js
@@ -2,7 +2,7 @@
 step1 Todo 메뉴 추가
 [*] 메뉴의 이름을 입력 받고 엔터 키를 치면 메뉴가 추가 된다
 [*] 추가 되는 메뉴의 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"><ul/> 안에 삽입 되야 한다
-[] 총 메뉴 갯수를 count 하여 상단에 보여 준다 => count total
+[*] 총 메뉴 갯수를 count 하여 상단에 보여 준다 => count total
 [] 메뉴가 추가 되고 나면 , input은 반드시 초기화 된다
 [] 사용자 입력값이 빈 값이라면 추가 되지 않는다.
 
@@ -19,6 +19,7 @@ const userInput = document.querySelector('#espresso-menu-name');
 const userSubmitButton = document.querySelector('#espresso-menu-submit-button');
 const menuForm = document.querySelector('#espresso-menu-form');
 const espressoMenuList = document.querySelector('#espresso-menu-list');
+const menuCount = document.querySelector('.menu-count');
 
 
 
@@ -31,7 +32,6 @@ const app = function() {
     //Enter 키를 눌렀을 때 userInput 에 입력된 value 를 출력
     userInput.addEventListener('keypress' , (event) => {
         if(event.key === 'Enter'){
-            
             const userInputValue = userInput.value;
             const menuList = (value) => {
                 return `
@@ -53,19 +53,45 @@ const app = function() {
             }
             console.log(menuList(userInputValue));
             espressoMenuList.insertAdjacentHTML("beforeend",menuList(userInputValue));
+            //리스트를 작성할 때 마다 espressoMenuList에 있는 <li> 노드의 갯수 즉 , length 를 불러와 변수에 할당 후 갯수를 카운트 해준다.
+            const menuCounter = espressoMenuList.querySelectorAll('li').length;
+            menuCount.innerText = `총 ${menuCounter}개`;
         }
     });
 
     //submit 버튼을 클릭 했을 때 userInput 에 입력된 value 를 출력 
     userSubmitButton.addEventListener('click' , (event) => {
         while(userInput){
-            console.log(userInput.value);
+            const userInputValue = userInput.value;
+            const menuList = (value) => {
+                return `
+                <li class="menu-list-item d-flex items-center py-2">
+                <span class="w-100 pl-2 menu-name">${value}</span>
+                <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                >
+                    수정
+                </button>
+                <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                >
+                    삭제
+                </button>
+                </li>`;
+            }
+            console.log(menuList(userInputValue));
+            espressoMenuList.insertAdjacentHTML("beforeend",menuList(userInputValue));
+            const menuCounter = espressoMenuList.querySelectorAll('li').length;
+            menuCount.innerText = `총 ${menuCounter}개`;
             break;
         }
     });
 }
 
 app();
+
 
 
 
