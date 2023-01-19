7b9eb7526026cc8470778bd5b84ca3eb08835eaa
junyeoplee <jyeop920@gmail.com>
2023-01-20 04:30:59 +0900

    set 로컬스토리지
    로컬스토리지에 등록 하기

--- moonbucks-menu-main/src/js/index.js
+++ moonbucks-menu-main/src/js/index.js
@@ -1,24 +1,8 @@
-/* 요구 사항 
-step1 Todo 메뉴 추가
-[*] 메뉴의 이름을 입력 받고 엔터 키를 치면 메뉴가 추가 된다
-[*] 메뉴의 이름을 입력 받고 확인 버튼을 누르면 메뉴가 추가 된다
-[*] 추가 되는 메뉴의 마크업은 <ul id="espresso-menu-list" class="mt-3 pl-0"><ul/> 안에 삽입 되야 한다
-[*] 메뉴가 추가 되고 나면 , input은 반드시 초기화 된다
-[*] 사용자 입력값이 빈 값이라면 추가 되지 않는다.
 
 
-step2 Todo 메뉴 수정
-[*] 메뉴의 수정 버튼 클릭 이벤트를 받고 , 메뉴 수정하는 모달창이 뜬다
-[*] 모달창에서 신규 메뉴명을 입력 받고 , 확인 버튼을 누르면 메뉴가 수정 된다.
-
-step3 Todo 메뉴 삭제
-[*] 메뉴삭제 버튼 클릭 이벤트를 받고 , 메뉴 삭제 컨펌 모달창이 뜬다
-[*] 확인 버튼을 클릭 하면 메뉴가 삭제 된다
-
-step4 Todo 메뉴 count
-[*] 총 메뉴 갯수를 count 하여 상단에 보여 준다 => count total
-*/
-
+const setMenu = (menu) => {
+    localStorage.setItem('menu' , JSON.stringify(menu));
+}//로컬에 정보를 저장해주는  역할을 해준다 
 
 
 const app = function() {
@@ -30,6 +14,8 @@ const espressoMenuList = document.querySelector('#espresso-menu-list');
 
 
 const menuCount = document.querySelector('.menu-count');
+
+    const menu = [];//로컬과 화면에 표시해줄 정보를 담을 배열 선언
 
     //form 태그가 자동으로 전송 되는 것을 막아 준다
     menuForm.addEventListener('submit', (event) => {
@@ -45,8 +31,28 @@ const menuCount = document.querySelector('.menu-count');
     //엔터나 클릭이 됐을 때 실행될 메뉴 리스트 내용을 담은 함수
     const addMenuList = () => {
         const userInputValue = userInput.value;
+        menu.push({name: userInputValue});//위에 선언된 빈배열인 menu 에 인풋에 입력한 값을 푸쉬한다
+        const template = menu.map((menu) => {
+            //menu 배열에 메뉴 하나당 객체 하나로 키값이 담긴다 그 배열의 키의 값을 화면에 출력 하고 로컬에 보내준다
+            return `
+            <li class="menu-list-item d-flex items-center py-2">
+            <span class="w-100 pl-2 menu-name">${menu.name}</span>
+            <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+            >
+                수정
+            </button>
+            <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+            >
+                삭제
+            </button>
+            </li>`
+        }).join('');//새로운 배열로 생성되는 map 의 배열을 스트링으로 풀어 준다
         //메뉴 추가를 할때 실행할 함수
-        const menuList = (value) => {
+        /*const menuList = (value) => {
             return `
             <li class="menu-list-item d-flex items-center py-2">
             <span class="w-100 pl-2 menu-name">${value}</span>
@@ -63,16 +69,18 @@ const menuCount = document.querySelector('.menu-count');
                 삭제
             </button>
             </li>`;
-        }
+        }*/
+
         //userinput이 '' 이면 경고창을 띄우고 return 으로 실행을 종료 시켜 빈칸의 리스트가 작성이 안되게 한다
         if(userInputValue === ''){
             return alert('메뉴 이름을 작성해 주세요!');
         }
-        //console.log(menuList(userInputValue));
-        espressoMenuList.insertAdjacentHTML("beforeend",menuList(userInputValue));
+        espressoMenuList.innerHTML = template;//ul에 맵으로 맵핑해서 등록해준다 //insertAdjacentHTML('beforeend',menuList(userInputValue));
+        setMenu(menu);//로컬로 menu 배열을 보내준다
         //리스트를 작성할 때 마다 espressoMenuList에 있는 <li> 노드의 갯수 즉 , length 를 불러와 변수에 할당 후 갯수를 카운트 해준다.
         menuCounter();
         userInput.value = '';
+
     }
 
     const updateMenuName = (event) => {
@@ -86,7 +94,6 @@ const menuCount = document.querySelector('.menu-count');
             }else{
                 menuName.innerText = menuName.innerText;
             }
-            //console.log(event.target);
         }
     }
 
@@ -108,7 +115,6 @@ const menuCount = document.querySelector('.menu-count');
 
     //submit 버튼을 클릭 했을 때 userInput 에 입력된 value 를 출력 
     userSubmitButton.addEventListener('click' ,  addMenuList);
-//if 문 수정
     
     
     //메뉴 이름 수정,삭제 하는 함수 . 이벤트 위임을 통해 수정.삭제 버튼 기능 구현
