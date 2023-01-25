70c4e13ab5b59e2d7bcbfa68a392cd7d39646c12
junyeoplee <jyeop920@gmail.com>
2023-01-25 15:06:19 +0900

    Update index.js

--- moonbucks-menu-main/src/js/index.js
+++ moonbucks-menu-main/src/js/index.js
@@ -1,20 +1,24 @@
 
-/*
-step 1 => Todo localstorage Read & Write
-    [*] localStorage에 데이터를 저장한다
-    [*] 추가 , 수정 , 삭제 버튼을 클릭 하면 localStorage에 상태값이 저장 된다
-    [*] localStrage에 있는 데이터를 읽어 온다
-step 2 => 카테고리별 메뉴판 관리
-    [*] 에스프레소, 프라푸치노, 블렌디드, 티바나, 디저트 각각의 종류별로 메뉴판을 관리할 수 있게 만든다.
-step 3 => 페이지 최초 접근시 최초 데이터 Read & Rendering
-    [*] 페이지에 최초로 접근할 때는 에스프레소 메뉴가 먼저 읽어 온다
-    [*] 에스프레소 메뉴를 페이지에 표시 해준다
-step 4 => 품절 상태 관리 
-    [*] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
-    [*] 품절 버튼을 추가 한다
-    [*] 품절 ,수정 , 삭제 버튼을 클릭 하면 localStorage에 상태값이 저장 된다
-    [*] 클릭 이벤트에서 가장 가까운 li 태그의 class 속성 값에 sold-out을 추가 한다
-품절 상태 메뉴의 마크업  */
+    //html에 접근해서 태그들 변수에 할당 해서 쓰기
+    const menuForm = document.querySelector('#espresso-menu-form');
+    const userInput = document.querySelector('#espresso-menu-name');
+    const userSubmitButton = document.querySelector('#espresso-menu-submit-button');
+    const espressoMenuList = document.querySelector('#espresso-menu-list');
+    const navigationBar = document.querySelector('#nav');
+    const menuCount = document.querySelector('.menu-count');
+
+    //로컬과 화면에 표시해줄 정보를 담을 배열 선언(배열로 초기화 시켜주는 이유는 우리가 invalue를 어떤 데이터 형식으로 받아올지 모르기 때문에
+    // 어떤 데이터 형식이든 배열에 push를 해줘서 다른 사람과 작업을 할 때 상태를 배열로 관리를 하는 구나 라는 의도를 알려주기 위해 
+    //기존에 하나의 게시판만 관리 하면 돼서 배열로만 했지만 여러개의 카테고리를 관리 하기 위해 객체에 키 값 을 담는 형시으로 저장
+let menu = {
+    espresso: [],
+    frappuccino: [],
+    blended: [],
+    teavana: [],
+    desert: []
+};
+//메뉴의 키에 접근할 때 쓸 변수 선언 , 기본값은 첫 페이지인 espresso , 다른 카테고리에 접근할 때 마다 값을 변경하여 해당 카테고리에 접근할 수있게 let 으로 선언
+let currentCategory = 'espresso';
 
 const setMenu = (menu) => {
     localStorage.setItem('menu' , JSON.stringify(menu));
@@ -31,30 +35,8 @@ function init(){
         menu = getMenu();
     }
     render();
-    menuCounter();
 }
 
-    //html에 접근해서 태그들 변수에 할당 해서 쓰기
-const menuForm = document.querySelector('#espresso-menu-form');
-const userInput = document.querySelector('#espresso-menu-name');
-const userSubmitButton = document.querySelector('#espresso-menu-submit-button');
-const espressoMenuList = document.querySelector('#espresso-menu-list');
-const navigationBar = document.querySelector('#nav');
-
-const menuCount = document.querySelector('.menu-count');
-
-//기존에 하나의 게시판만 관리 하면 돼서 배열로만 했지만 여러개의 카테고리를 관리 하기 위해 객체에 키 값 을 담는 형시으로 저장
-let menu = {
-    espresso: [],
-    frappuccino: [],
-    blended: [],
-    teavana: [],
-    desert: []
-};
-
-let currentCategory = 'espresso';
-    //로컬과 화면에 표시해줄 정보를 담을 배열 선언(배열로 초기화 시켜주는 이유는 우리가 invalue를 어떤 데이터 형식으로 받아올지 모르기 때문에
-                    // 어떤 데이터 형식이든 배열에 push를 해줘서 다른 사람과 작업을 할 때 상태를 배열로 관리를 하는 구나 라는 의도를 알려주기 위해 
     //form 태그가 자동으로 전송 되는 것을 막아 준다
     menuForm.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -62,7 +44,7 @@ let currentCategory = 'espresso';
     
     //수정 또는 삭제 했을 때 메뉴 갯수를 카운터 해주는 함수
     const menuCounter = () => {
-        const menuCounted = espressoMenuList.querySelectorAll('li').length;
+        const menuCounted = menu[currentCategory].length;
         menuCount.innerText = `총 ${menuCounted}개`;
     }
 
@@ -96,7 +78,8 @@ let currentCategory = 'espresso';
         </button>
         </li>`
     }).join('');
-    espressoMenuList.innerHTML = template;//ul에 맵으로 맵핑해서 등록해준다 
+    espressoMenuList.innerHTML = template;//ul에 맵으로 맵핑해서 등록해준다
+    menuCounter(); 
 };//새로운 배열로 생성되는 map 의 배열을 스트링으로 풀어 준다
 
     //엔터나 클릭이 됐을 때 실행될 메뉴 리스트 내용을 담은 함수
@@ -110,10 +93,7 @@ let currentCategory = 'espresso';
         menu[currentCategory].push({name:userInputValue});
         render();
         setMenu(menu);//로컬로 menu 배열을 보내준다
-        //리스트를 작성할 때 마다 espressoMenuList에 있는 <li> 노드의 갯수 즉 , length 를 불러와 변수에 할당 후 갯수를 카운트 해준다.
-        menuCounter();
         userInput.value = '';
-
     }
     
     const updateMenuName = (event) => {
@@ -125,11 +105,11 @@ let currentCategory = 'espresso';
             const modifiedName = prompt('메뉴 수정' , menuName.innerText);
 
             if(modifiedName){
-                menuName.innerText = modifiedName;
                 //menuId 는 배열의 index 값과 동일하기 때문에 menu의 인덱스에 접근해 해당 name 키에 있는 값을 수정한 값으로 로컬에서 바꿔 준다.
-                menu[currentCategory][menuId] = modifiedName;
+                menu[currentCategory][menuId] = {name: modifiedName};
                 //바꿔준 값을 local 에 보내주면 바뀐다 
                 //데이터를 바꿔 줄때는 항상 최소한의 꼬이지 않게 깔끔하게 로직을 짠다 한번에 모든 함수에서 해결 하려 하지 말고 역할을 나눠 준다
+                render();
                 setMenu(menu);
             }else{
                 menuName.innerText = menuName.innerText;
@@ -142,10 +122,9 @@ let currentCategory = 'espresso';
 
         if(confirm('정말 삭제 하시겠습니까?')){
             menu[currentCategory].splice(menuId, 1);
-            setMenu(menu);
             render();
+            setMenu(menu);
             console.log(menuId);
-            menuCounter();
             //menu 배열에 접근해 splice 로 삭제 버튼을 누른 해당 index 에 요소 를 지운다 그 후 setMenu로 로컬에 전달 한다
             return;
         }
@@ -153,11 +132,10 @@ let currentCategory = 'espresso';
 
     const soldOutMenuName = (event) => {
         const menuId = event.target.closest('li').dataset.menuId;
-        console.log("여기")
         menu[currentCategory][menuId].soldOut = menu[currentCategory][menuId].soldOut  === true ? false : true;
-        console.log(menu[currentCategory][menuId])
-        setMenu(menu);
+        console.log(menu[currentCategory][menuId]);
         render();
+        setMenu(menu);
     }
 
     //Enter 키를 눌렀을 때 userInput 에 입력된 value 를 출력
@@ -193,12 +171,8 @@ let currentCategory = 'espresso';
             currentCategory = categoryName;
             categoryTitle.innerText = `${event.target.innerText} 메뉴 관리 `;
             render();
-            menuCounter();
         }
     })
-
-
-
 
 
 init();
